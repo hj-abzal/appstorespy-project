9954e4f6bd9ae14197b488cc74692615e72fd30c
abzal <abzal008suan@gmail.com>
2021-11-27 22:29:26 +0600

    add material ui and refactor todolist.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
+import { AppBar, Container, Grid, IconButton, Paper, Toolbar, Typography } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
 import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { AddItemForm } from '../components/AddItemForm';
+import { AddItemForm } from '../components/AddItemForm/AddItemForm';
 import { TasksStateType } from '../features/Tasks/tasks-reducer';
 import { Todolist } from '../features/Todolists/Todolist';
 import { addTodolistAC, TodolistType } from '../features/Todolists/todolist-reducer';
@@ -9,26 +11,54 @@ import { AppRootStateType } from './store';
 
 function App() {
   const dispatch = useDispatch();
-  const todolists = useSelector<AppRootStateType, TodolistType[]>( state => state.todolists);
-  const tasks = useSelector<AppRootStateType, TasksStateType>( state => state.tasks);
+  const todolists = useSelector<AppRootStateType, TodolistType[]>(state => state.todolists);
+  const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks);
   const addTodolist = useCallback((title: string) => {
     dispatch(addTodolistAC(title))
   }, [dispatch])
 
   return (
     <div className="App">
-      Todolist
-      <AddItemForm addItem={addTodolist} />
-      {
-        todolists.map( t => {
-          return <Todolist
-          key={t.id}
-          id={t.id}
-          title={t.title}
-          tasks={tasks[t.id]} 
-          />
-        })
-      }
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h6">
+            AppStoreSpy
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container fixed>
+        <Grid container style={{ padding: '20px' }}>
+          <AddItemForm addItem={addTodolist} />
+        </Grid>
+        <Grid container spacing={3}>
+          {
+            todolists.map(t => {
+              let allTodolistTasks = tasks[t.id];
+              let tasksForTodolist = allTodolistTasks;
+
+              if (t.filter === "active") {
+                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
+              }
+              if (t.filter === "completed") {
+                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
+              }
+              return <Grid item key={t.id}>
+                <Paper style={{ padding: '10px' }}>
+                  <Todolist
+                    id={t.id}
+                    title={t.title}
+                    tasks={tasksForTodolist}
+                    filter={t.filter}
+                  />
+                </Paper>
+              </Grid>
+            })
+          }
+        </Grid>
+      </Container>
     </div>
   );
 }
--- src/features/Todolists/Todolist.tsx
+++ src/features/Todolists/Todolist.tsx
@@ -1,35 +1,69 @@
-import React, { ChangeEvent } from 'react';
+import { Button, IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import React from 'react';
 import { useDispatch } from 'react-redux';
-import { AddItemForm } from '../../components/AddItemForm';
+import { AddItemForm } from '../../components/AddItemForm/AddItemForm';
+import { EditableSpan } from '../../components/EditableSpan/EditableSpan';
 import { Task } from '../Tasks/Task';
 import { addTaskAC, TaskType } from '../Tasks/tasks-reducer';
+import { changeTodolistFilterAC, FilterValuesType, removeTodolistAC } from './todolist-reducer';
 
 
 type PropsType = {
     id: string
     title: string
     tasks: Array<TaskType>
+    filter: FilterValuesType
 }
 
 export function Todolist(props: PropsType) {
-    
+
     const dispatch = useDispatch();
 
     const addTask = (title: string) => {
-       dispatch(addTaskAC(title, props.id))
+        dispatch(addTaskAC(title, props.id))
     }
 
+    const removeTodolist = () => {
+        dispatch(removeTodolistAC(props.id))
+    }
 
+    const onAllClickHandler = () => {
+        dispatch(changeTodolistFilterAC(props.id, 'all'))
+    }
+    const onActiveClickHandler = () => {
+        dispatch(changeTodolistFilterAC(props.id, "active"))
+    }
+    const onCompletedClickHandler = () => {
+        dispatch(changeTodolistFilterAC(props.id, "completed"))
+    }
 
     return <div>
-        <h3>{props.title}</h3>
+        <h3><EditableSpan value={props.title} onChange={() => {}} />
+            <IconButton onClick={removeTodolist}>
+                <Delete />
+            </IconButton>
+        </h3>
         <AddItemForm addItem={addTask} />
         <div>
             {
-                props.tasks.map( t => {
-                    return <Task key={t.id} task={t} todolistId={props.id}/>
-                })
+                props.tasks.map(t => <Task key={t.id} task={t} todolistId={props.id} />)
             }
+        </div>
+        <div style={{ paddingTop: '10px' }}>
+            <Button variant={props.filter === 'all' ? 'outlined' : 'text'}
+                onClick={onAllClickHandler}
+                color={'default'}
+            >All
+            </Button>
+            <Button variant={props.filter === 'active' ? 'outlined' : 'text'}
+                onClick={onActiveClickHandler}
+                color={'primary'}>Active
+            </Button>
+            <Button variant={props.filter === 'completed' ? 'outlined' : 'text'}
+                onClick={onCompletedClickHandler}
+                color={'secondary'}>Completed
+            </Button>
         </div>
     </div>
 }
